543b15c1f72dbc138c7b23acc9f5a9031976e9d7
sneha-tiwari06 <114509946+sneha-tiwari06@users.noreply.github.com>
2025-02-23 13:02:59 +0530

    user id

--- model/orderModel.js
+++ model/orderModel.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  selectedItems: {
-    type: String, // Stored as a stringified JSON for flexibility
-    required: true,
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to User model
+      required: true,
+      ref: "User", // Assuming you have a User model
+    },
+    selectedItems: {
+      type: String, // Stored as a stringified JSON for flexibility
+      required: true,
+    },
+    selectedDate: {
+      type: String,
+      required: true,
+    },
+    selectedTimeSlot: {
+      type: String,
+      required: true,
+    },
+    address: {
+      firstname: { type: String, required: true },
+      lastname: { type: String, required: true },
+      emailId: { type: String, required: true },
+      fullAddress: { type: String, required: true },
+      pincode: { type: Number, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+    },
   },
-  selectedDate: {
-    type: String,
-    required: true,
-  },
-  selectedTimeSlot: {
-    type: String,
-    required: true,
-  },
-  address: {
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
-    emailId: { type: String, required: true },
-    fullAddress: { type: String, required: true },
-    pincode: { type: Number, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", orderSchema);
